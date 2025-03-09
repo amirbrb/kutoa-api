431e85e249ee40d0e34fcbb9593b645ec3991006
amir mishori <amir.mishori@clinch.co>
2025-03-09 08:01:49 +0200

    config

--- src/utils/configuration/readConfiguration.ts
+++ src/utils/configuration/readConfiguration.ts
@@ -24,6 +24,21 @@ interface AppConfiguration extends Configuration {
 }
 
 export default function readConfiguration(): AppConfiguration {
-  const config = require('../../app.config.json');
-  return JSON.parse(JSON.stringify(config));
+  console.log(process.env);
+
+  return {
+    appUrl: `${process.env.APP_HOST}:${process.env.APP_PORT}`,
+    host: process.env.APP_HOST,
+    port: parseInt(process.env.APP_PORT),
+    sqlConfig: {
+      host: process.env.SQL_HOST,
+      user: process.env.SQL_USER,
+      password: process.env.SQL_PASSWORD,
+      databaseName: process.env.SQL_DATABASE,
+      port: parseInt(process.env.SQL_PORT),
+    },
+    emailConfig: {
+      appPassword: process.env.EMAIL_APP_PASSWORD,
+    },
+  };
 }
